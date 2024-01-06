77187bc2046636b2b6e6f66b9e003844961a76de
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-06 10:19:20 +0700

    * Spring Web App Club - Create Club Service - Create ClubService Impl

---
+++ src/main/java/com/zuhaproject/web/service/ClubService.java
@@ -0,0 +1,9 @@
+package com.zuhaproject.web.service;
+
+import com.zuhaproject.web.dto.ClubDto;
+
+import java.util.List;
+
+public interface ClubService {
+    List<ClubDto> findAllClub();
+}
---
+++ src/main/java/com/zuhaproject/web/service/impl/ClubServiceImpl.java
@@ -0,0 +1,39 @@
+package com.zuhaproject.web.service.impl;
+
+import com.zuhaproject.web.dto.ClubDto;
+import com.zuhaproject.web.models.Club;
+import com.zuhaproject.web.repository.ClubRepository;
+import com.zuhaproject.web.service.ClubService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ClubServiceImpl implements ClubService {
+
+    private ClubRepository clubRepository;
+
+    @Autowired
+    public ClubServiceImpl(ClubRepository clubRepository) {
+        this.clubRepository = clubRepository;
+    }
+
+    @Override
+    public List<ClubDto> findAllClub() {
+        List<Club> clubs = clubRepository.findAll();
+        return clubs.stream().map((club -> mapToClubDto(club))).collect(Collectors.toList());
+    }
+
+    private ClubDto mapToClubDto(Club club) {
+        ClubDto clubDto = ClubDto.builder()
+                .id(club.getId())
+                .title(club.getTitle())
+                .photoUrl(club.getPhotoUrl())
+                .content(club.getContent())
+                .createdOn(club.getCreatedOn())
+                .updatedOn(club.getUpdatedOn())
+                .build();
+        return clubDto;
+    }
+
+}
