3dfc5413b904c68276e40bc9da1a9005baba995d
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-06 09:44:35 +0700

    Initial commit

---
+++ src/main/java/com/zuhaproject/web/SpringWebClubApplication.java
@@ -0,0 +1,13 @@
+package com.zuhaproject.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebClubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zuhaproject/web/SpringWebClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuhaproject.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
