a99ed4462f67569163b2f7f549f2f5a8c8a39b8d
Aziz Alfa <azizalfauzi65@gmail.com>
2024-01-07 10:45:02 +0700

    * Spring Web App Club - Adding create club - Adding function in controller GetMapping clubs/new - Adiing function in controller PostMapping clubs/new - Adding form and handle input - And much more

--- src/main/java/com/zuhaproject/web/controller/ClubController.java
+++ src/main/java/com/zuhaproject/web/controller/ClubController.java
@@ -1,11 +1,14 @@
 package com.zuhaproject.web.controller;
 
 import com.zuhaproject.web.dto.ClubDto;
+import com.zuhaproject.web.models.Club;
 import com.zuhaproject.web.service.ClubService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -25,4 +28,16 @@ public class ClubController {
         return "club-list";
     }
 
+    @GetMapping("/clubs/new")
+    public String createClubForm(Model model) {
+        Club club = new Club();
+        model.addAttribute("club", club);
+        return "club-create";
+    }
+
+    @PostMapping("/clubs/new")
+    public String saveClub(@ModelAttribute("club") Club club) {
+        clubService.saveClub(club);
+        return "redirect:/clubs";
+    }
 }
--- src/main/java/com/zuhaproject/web/service/ClubService.java
+++ src/main/java/com/zuhaproject/web/service/ClubService.java
@@ -1,9 +1,12 @@
 package com.zuhaproject.web.service;
 
 import com.zuhaproject.web.dto.ClubDto;
+import com.zuhaproject.web.models.Club;
 
 import java.util.List;
 
 public interface ClubService {
     List<ClubDto> findAllClub();
+
+    Club saveClub(Club club);
 }
