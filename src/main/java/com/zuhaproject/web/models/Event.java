bef3d47164d240efa15b50a7506886d79fbee150
Aziz Alfa <azizalfauzi65@gmail.com>
2024-02-03 09:38:34 +0700

    * Spring Web App Club - creating model Event.java

---
+++ src/main/java/com/zuhaproject/web/models/Event.java
@@ -0,0 +1,39 @@
+package com.zuhaproject.web.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String type;
+
+    private LocalDateTime startTime;
+
+    private LocalDateTime endTime;
+
+    @CreationTimestamp
+    private LocalDateTime createdOn;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedOn;
+}
